2cdf63ce4ebe405670951ebf6c5499bae6a709b5
isaac harris <isaachrr4@gmail.com>
2022-09-19 13:56:24 -0400

    making changes

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,6 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rajshekhar26.github.io/cleanfolio',
+  homepage: 'https://isaachrr4.github.io/welcome-to-forever-3',
   title: 'IH.',
 }
 
@@ -10,10 +10,10 @@ const about = {
   role: 'Full Stack Developer',
   description:
     'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-  resume: 'https://example.com',
+  resume: 'https://drive.google.com/file/d/1CvuAP8yW6ZFqvtmjOHxyy46P75aHV5uP/view?usp=sharing',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://www.linkedin.com/in/isaac-harris-608644209',
+    github: 'https://github.com/isaachrr4',
   },
 }
 
@@ -21,18 +21,18 @@ const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
-    name: 'Project 1',
+    name: 'Crypto-Tycoon',
     description:
       'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+    stack: ['HTML5', 'CSS', 'React.js', 'JavaScript'],
+    sourceCode: 'https://github.com/AnthonySzymczak/Crypto-Tycoon',
+    livePreview: 'https://anthonyszymczak.github.io/Crypto-Tycoon/',
   },
   {
-    name: 'Project 2',
+    name: 'PokeDecks',
     description:
       'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
+    stack: ['Java 8', 'TypeScript', 'React', 'Spring Boot'],
     sourceCode: 'https://github.com',
     livePreview: 'https://github.com',
   },
@@ -60,12 +60,15 @@ const skills = [
   'Git',
   'CI/CD',
   'Jest',
-  'Enzyme',
+  'Spring Boot',
+  'Java 8',
+  'AWS EC2',
+  'AWS S3'
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'isaacharris90@yahoo.com',
 }
 
 export { header, about, projects, skills, contact }
